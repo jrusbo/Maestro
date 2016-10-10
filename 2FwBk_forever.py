576c70e8565a8ca20af94f401144f127f51458d4
brjapon <brjapon@psykia.com>
2016-10-10 17:50:52 +0200

    Anadido motor izq y drch en 2FwBk_forever.py

--- 2FwBk_forever.py
+++ 2FwBk_forever.py
@@ -5,19 +5,22 @@ import maestro as m
 
 s= m.Controller()
 
+servo_izq=0
+servo_dcho=1
+
 while True:
 	# 1. Arrancamos maquinas FW!!!
-	s.setTarget(0,1)
-	s.setTarget(1,-1)
+	s.setTarget(servo_izq,1)
+	s.setTarget(servo_dcho,-1)
 	
 	time.sleep(3)
 	
 	# 2. Paramos maquinas!!!
-	s.setTarget(0,0)
-	s.setTarget(1,0)
+	s.setTarget(servo_izq,0)
+	s.setTarget(servo_dcho,0)
 	
 	# 3. Revertimos maquinas BW!!!
-	s.setTarget(0,-1)
-	s.setTarget(1,1)
+	s.setTarget(servo_izq,-1)
+	s.setTarget(servo_dcho,1)
 
 	time.sleep(3)
